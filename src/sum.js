fabc8659b636691db5c5614f97c864ef75be753c
leoboyerbx <contact@leoboyer.fr>
2020-09-17 09:46:58 +0200

    Fist test for travis ci

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,10 @@
 // Implement the sum function here
+function sum (number1, number2) {
+    if (typeof number1 === 'number' && typeof number2 === 'number') {
+        return (number1 + number2)
+    }
+    return false
+}
+
+
+export default sum
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,24 @@
 // Add the unit tests of the sum function here
+import sum from "../src/sum"
+
+describe('Function operates basic sum', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+
+    test('adds 5 + 8 to equal 13', () => {
+        expect(sum(5, 8)).toBe(13)
+    })
+})
+
+describe('Type validation', () => {
+    test('Checks if arg 1 is number', () => {
+        expect(sum('5', 1)).toBe(false)
+    })
+    test('Checks if arg 2 is number', () => {
+        expect(sum(5, '1')).toBe(false)
+    })
+    test('Checks if both args are numbers', () => {
+        expect(sum('5', '1')).toBe(false)
+    })
+})
